62c80458822d517fec40225f69785f4d4661d7ee
dipakrathod09 <dipakrathod20060909@gmail.com>
2025-12-28 20:49:19 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "dipak"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "Hii"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+let someNumber = 50
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
