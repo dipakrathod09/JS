37194e2bcf9e645b0be3ff7d0e1ec0eba6cb0675
dipakrathod09 <dipakrathod20060909@gmail.com>
2025-12-28 19:55:24 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) We are using Node.js Not Browser
+console.log(3+3);
+
+let name = "Dipak"
+let age = 19
+let isLoggedIn = false
+// number, bigint, boolean, null(stand-alone value), undefined, symbol(unique)
+
+// object 
+
+console.log(typeof null); // Object
+console.log(typeof undefined); //unique
