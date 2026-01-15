7e107db6ba8457507313795927a0183a74718480
dipakrathod09 <dipakrathod20060909@gmail.com>
2026-01-15 15:11:41 +0000

    Arrays 2 Done

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,29 @@
+const marvel_heros = ["Thor","Iron Man","Spider Man"]
+const dc_heros = ["Super Man","Bat Man","Flash"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// All_heros = marvel_heros.concat(dc_heros)
+// console.log(All_heros);
+
+const All_new_heros = [...marvel_heros,...dc_heros]
+// console.log(All_new_heros);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("Dipak"));
+console.log(Array.from("Dipak"));
+console.log(Array.from({name:"Dipak"})); //Note
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
